07f240850c2f951f43b77fbad3aab5ecde4cddec
giridharan s <160309516+gdharan361@users.noreply.github.com>
2024-08-28 11:23:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GetEmployee from "./Components/GetEmployee";
-import AddEmployeeForm from "./components/AddEmployeeForm";
+import AddEmployeeForm from "./Components/AddEmployeeForm";
 import EditEmployeeForm from "./Components/EditEmployeeForm";
 
 const App = () => {
